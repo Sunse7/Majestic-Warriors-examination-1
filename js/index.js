dc103cfff741964aa2339b77680842571a6880e2
Sunse7 <sunsetluna12@gmail.com>
2023-01-24 10:24:53 +0100

    Wip

--- js/index.js
+++ js/index.js
@@ -23,19 +23,34 @@ let userTries = 0;
 let keypressed;
 
 
+
 document.onkeydown = function (e) {
     keypressed = e.key;
     console.log(keypressed, 'hej');
 
     if (randomWord.includes(keypressed)) {
         console.log(`The word contained ${keypressed}`);
-        guessedLetters.push(keypressed);
+        guessedLetters.unshift(keypressed);
         letters.splice();
+
+        guessedLetters.forEach(letter => {
+            let el1 = document.createElement('li');
+            el1.innerHTML = `${guessedLetters[0]}`;
+            document.querySelector('.word').appendChild(el1);
+        });
+
     }
     else {
         nrOfTries--;
         guessedLetters.push(keypressed);
         letters.splice();
+        el1.innerHTML = `${guessedLetters[0]}`;
+        document.querySelector('.nomatch').appendChild(el1);
+        document.querySelector('figure').classList.add('scaffold');
         console.log(nrOfTries, 'nrOfTries');
     }
 };
+
+
+
+

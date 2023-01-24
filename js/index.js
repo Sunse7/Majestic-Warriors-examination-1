1710634c6e5333a8018f1bdfa860766b0bbee643
Sunse7 <sunsetluna12@gmail.com>
2023-01-24 09:07:23 +0100

    Wip

--- js/index.js
+++ js/index.js
@@ -7,3 +7,33 @@
  document.querySelector('figure').classList.add('legs')
 
  */
+
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let guessedLetters = [];
+
+let words = ['dog', 'cat', 'raccoon'];
+
+let randomWord = words[Math.floor(Math.random() * words.length)];
+console.log(randomWord, 'random');
+
+let nrOfTries = 5;
+let userTries = 0;
+let keypressed;
+
+
+document.onkeydown = function (e) {
+    keypressed = e.key;
+    console.log(keypressed, 'hej');
+
+    if (randomWord.includes(keypressed)) {
+        console.log(`The word contained ${keypressed}`);
+        guessedLetters.push(keypressed);
+    }
+    else {
+        nrOfTries--;
+        guessedLetters.push(keypressed);
+        console.log(nrOfTries, 'nrOfTries');
+    }
+};

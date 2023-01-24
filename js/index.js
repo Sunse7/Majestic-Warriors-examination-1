74b4c7ea66faf8d22e362fbc9da78edd62367473
sparrro <116090214+sparrro@users.noreply.github.com>
2023-01-24 13:05:22 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,8 +8,8 @@
 
  */
 
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+ let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 let guessedLetters = [];
 
@@ -18,6 +18,12 @@ let words = ['dog', 'cat', 'raccoon'];
 let randomWord = words[Math.floor(Math.random() * words.length)];
 console.log(randomWord, 'random');
 
+const wordEl = document.querySelector('.word')
+for(let i = 0; i<randomWord.length; i++){
+ let emptyLetter = document.createElement('li');
+ wordEl.appendChild(emptyLetter)
+}
+
 let nrOfTries = 5;
 let userTries = 0;
 let keypressed;
@@ -25,13 +31,13 @@ let keypressed;
 
 
 document.onkeydown = function (e) {
-    keypressed = e.key;
-    console.log(keypressed, 'hej');
+ keypressed = e.key;
+ console.log(keypressed, 'hej');
 
-    if (randomWord.includes(keypressed)) {
-        console.log(`The word contained ${keypressed}`);
-        guessedLetters.unshift(keypressed);
-        letters.splice();
+ if (randomWord.includes(keypressed)) {
+     console.log(`The word contained ${keypressed}`);
+     guessedLetters.unshift(keypressed);
+     letters.splice();
 
 //        guessedLetters.forEach(letter => {
 //            let el1 = document.createElement('li');
@@ -39,23 +45,29 @@ document.onkeydown = function (e) {
 //            document.querySelector('.word').appendChild(el1);
 //        });
 
-        for(let i = 0; i<randomWord.length; i++){
-            if(randomWord[i] == keypressed){
-                let el1 = document.createElement('li');
-                el1.innerHTML = `${keypressed}`;
-                document.querySelector('.word').insertBefore(el1, document.querySelector('.word').children[randomWord.indexOf(keypressed)])
-            }
-        }
+//        for(let i = 0; i<randomWord.length; i++){
+//            if(randomWord[i] == keypressed){
+//                let el1 = document.createElement('li');
+//                el1.innerHTML = `${keypressed}`;
+//                document.querySelector('.word').insertBefore(el1, document.querySelector('.word').children[randomWord.indexOf(keypressed)])
+//            }
+//        }
 
-    }
-    else {
-        nrOfTries--;
-        guessedLetters.push(keypressed);
-        letters.splice();
-        el1.innerHTML = `${guessedLetters[0]}`;
-        document.querySelector('.nomatch').appendChild(el1);
-        document.querySelector('figure').classList.add('scaffold');
-        console.log(nrOfTries, 'nrOfTries');
-    }
+             for(let i = 0; i<wordEl.children.length; i++){
+                 if(randomWord[i]==keypressed){
+                     wordEl.children[i].innerHTML = keypressed
+                 }
+             }
+
+ }
+ else {
+     nrOfTries--;
+     guessedLetters.push(keypressed);
+     letters.splice();
+     el1.innerHTML = `${guessedLetters[0]}`;
+     document.querySelector('.nomatch').appendChild(el1);
+     document.querySelector('figure').classList.add('scaffold');
+     console.log(nrOfTries, 'nrOfTries');
+ }
 };
 
